8e0cafa042a7e77d76b928a6ab3af59090603bd4
Jon Schneider <jon.schneider@gmail.com>
2020-08-21 12:30:45 -0400

    Add test for outputting table data

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -5,9 +5,14 @@ class MyTable < Rulers::Model::SQLite; end
 STDERR.puts MyTable.schema.inspect
 
 # Create a row
-mt = MyTable.create 'title' => 'It happened!', 'posted' => 1, 'body' => 'It did!'
-mt = MyTable.create 'title' => 'I saw it!'
+mt = MyTable.create 'title' => 'I saw it again!'
 
 puts "Count: #{MyTable.count}"
+
+top_id = mt['id'].to_i
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id['title']}."
+end
 
 
